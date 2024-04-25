eb6179161e356eeb6cbfe6640c69074bb680b41f
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2024-04-25 21:44:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mailboxIcon = document.getElementById('mailbox-icon');
+
+    mailboxIcon.addEventListener('click', function (event) {
+        event.preventDefault();
+        const defaultEmail = 'dilujilshadvkd@gmail.com'; // Default email address
+        const emailInput = document.getElementById('email').value;
+        const mailtoUrl = `mailto:${defaultEmail}?subject=Mailbox&body=Email: ${encodeURIComponent(emailInput)}`;
+        window.location.href = mailtoUrl;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const mailboxIcon = document.getElementById('submit');
+
+    mailboxIcon.addEventListener('click', function (event) {
+        event.preventDefault();
+        const defaultEmail = 'dilujilshadvkd@gmail.com'; // Default email address
+        const emailInput = document.getElementById('email').value;
+        const mailtoUrl = `mailto:${defaultEmail}?subject=Mailbox&body=Email: ${encodeURIComponent(emailInput)}`;
+        window.location.href = mailtoUrl;
+    });
+});
